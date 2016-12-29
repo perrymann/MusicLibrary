0696ee724c8962092e58b47e37fa0dce51632aeb
perrymann <perttu.nikander@cs.helsinki.fi>
2016-12-29 19:38:19 +0200

    default controllerista poistettu ylimääräinen roska

--- src/main/java/MusicLibrary/controller/DefaultController.java
+++ src/main/java/MusicLibrary/controller/DefaultController.java
@@ -1,17 +1,10 @@
 package MusicLibrary.controller;
 
-
-import MusicLibrary.domain.Account;
-//import MusicLibrary.domain.Album;
-//import MusicLibrary.domain.Artist;
-//import MusicLibrary.domain.StyleTag;
 import MusicLibrary.repository.AccountRepository;
 import MusicLibrary.repository.AlbumRepository;
 import MusicLibrary.repository.ArtistRepository;
 import MusicLibrary.repository.StyleTagRepository;
-import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
