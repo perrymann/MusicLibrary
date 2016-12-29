66eb15d89b2e9f8087f156b09a70c405d1cb7eb7
perrymann <perttu.nikander@cs.helsinki.fi>
2016-12-29 20:06:23 +0200

    default controller siirtokuntoon

--- src/main/java/MusicLibrary/controller/DefaultController.java
+++ src/main/java/MusicLibrary/controller/DefaultController.java
@@ -1,28 +1,11 @@
 package MusicLibrary.controller;
 
-import MusicLibrary.repository.AccountRepository;
-import MusicLibrary.repository.AlbumRepository;
-import MusicLibrary.repository.ArtistRepository;
-import MusicLibrary.repository.StyleTagRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DefaultController {
     
-    @Autowired
-    private AlbumRepository albumRepo;
-    
-    @Autowired
-    private ArtistRepository artistRepo;
-    
-    @Autowired
-    private StyleTagRepository styleTagRepo;
-    
-    @Autowired
-    private AccountRepository accountRepo;
-   
     @RequestMapping("*")
     public String handleDefault() {
         return "redirect:/artists";
